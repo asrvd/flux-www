f17cd14c70ce40a279d419f05b25ac5fe5a4c55a
asrvd <asheeshh@duck.com>
2025-04-25 21:33:21 +0530

    add docs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,25 @@
-import Link from 'next/link';
+import { cn } from "@/lib/utils";
+import { AnimatedGridPattern } from "../components/grid";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+    <main className="flex flex-1 flex-col justify-center text-center p-4 md:p-6 relative overflow-y-hidden">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-6xl md:text-8xl font-serif italic">Flux</h1>
+        <p className="text-sm md:text-base lg:text-lg font-sans">
+          / mcp server on steroids built for AO and Arweave /
+        </p>
+      </div>
+      <AnimatedGridPattern
+        numSquares={30}
+        maxOpacity={0.1}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-10%] h-[200%] skew-y-12 overflow-hidden top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        )}
+      />
     </main>
   );
 }
